e826edfe7da636b418e4f394c2047645529fc9ce
Younes-Belaabda <belaabda.youness@gmail.com>
2023-11-27 16:28:25 +0100

    Enhancement code

--- app/Http/Controllers/ContratController.php
+++ app/Http/Controllers/ContratController.php
@@ -88,7 +88,7 @@ class ContratController extends Controller
 
     public function all(){
         // $contrats = Auth::user()->contrats->reverse();
-        $contrats = \App\Models\Contrat::paginate(10);
+        $contrats = \App\Models\Contrat::latest()->paginate(10);
         return view('pages.agent.contract.all' , compact('contrats'));
     }
 
