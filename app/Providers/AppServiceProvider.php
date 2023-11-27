17c41112745a7212eef485e5423ea7aada57807e
Younes-Belaabda <belaabda.youness@gmail.com>
2023-11-27 16:22:06 +0100

    Enhancement code

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,7 @@ namespace App\Providers;
 
 use App\Models\Contrat;
 use App\Observers\ContratObserver;
-use Illuminate\Pagination\Paginator;
+// use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +23,5 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Contrat::observe(ContratObserver::class);
-        Paginator::useBootstrapFour();
     }
 }
